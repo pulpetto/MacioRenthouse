4ac6afa3d316f44b2b2037a7cd0aeffaa5c2cee0
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-08-18 18:37:06 +0200

    add firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        projectId: 'macio-renthouse',
+        appId: '1:246621205762:web:dc4b45dfcffe5af9757e1b',
+        storageBucket: 'macio-renthouse.appspot.com',
+        apiKey: 'AIzaSyAYowJDCY4p5NSpyjbYPwJfbZ4lcEoCKDM',
+        authDomain: 'macio-renthouse.firebaseapp.com',
+        messagingSenderId: '246621205762',
+        measurementId: 'G-B2PCZZ7P0B',
+    },
+};
