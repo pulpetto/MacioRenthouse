454bbcf8857d70d7378452d4f7363315ef629707
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-08-25 11:16:17 +0200

    validate username login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+    FormGroup,
+    FormControl,
+    Validators,
+    AbstractControl,
+    ValidationErrors,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
@@ -16,10 +22,30 @@ export class LoginComponent {
     }
 
     loginForm = new FormGroup({
-        username: new FormControl('', [Validators.required]),
+        username: new FormControl(
+            '',
+            [Validators.required],
+            this.usernameValidator.bind(this)
+        ),
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required]),
     });
+
+    usernameValidator(
+        control: AbstractControl
+    ): Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            if (
+                !this.userService.users.some((user) => {
+                    return user.username === control.value;
+                })
+            ) {
+                resolve({ usernameDoestExist: true });
+            } else {
+                resolve(null);
+            }
+        });
+    }
 
     onLogIn() {
         // check if user has account already
