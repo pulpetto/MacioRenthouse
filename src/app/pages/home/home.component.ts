2818df641cfa5ae05de8cae4da8a42232948e132
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-09-02 10:33:08 +0200

    home page navigation when logged

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    constructor(private userService: UserService, private router: Router) {}
+    isLoggedIn!: boolean;
 
+    ngOnInit() {
+        this.userService.isLoggedIn().subscribe((isLoggedIn) => {
+            this.isLoggedIn = isLoggedIn;
+        });
+    }
+
+    onOfferAdd() {
+        if (this.isLoggedIn === false) {
+            this.router.navigate(['/login']);
+        } else {
+            // or navigate directly to offer creator panel
+            this.userService.navigateToDashboard();
+        }
+    }
 }
