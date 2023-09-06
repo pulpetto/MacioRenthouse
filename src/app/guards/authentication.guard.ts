c728cdf1b8aacf1def9bd6f838af9f38c729ffba
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-09-06 06:24:56 +0200

    Update authentication.guard.ts

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -19,49 +19,14 @@ export const authenticationGuard = (
             (user) => user.username === route.params['username']
         );
 
-        userService.getUser().subscribe((loggedUser) => {
+        return userService.getUser().subscribe((loggedUser) => {
             if (JSON.stringify(loggedUser) === JSON.stringify(userFromUrl[0])) {
                 return true;
             } else {
+                router.navigate(['404']);
                 return false;
             }
         });
-
-        // if (
-        //     // users.some((user) => {
-        //     //     return user.username === route.params['username'];
-        //     // })
-        //     userService.getUser().subscribe((loggedUser) => {
-        //         if (
-        //             JSON.stringify(loggedUser) ===
-        //             JSON.stringify(userFromUrl[0])
-        //         ) {
-        //             return true;
-        //         } else {
-        //             return false;
-        //         }
-        //     })
-        // ) {
-        //     return true;
-        //     // const userFromUrl = users.filter(
-        //     //     (user) => user.username === route.params['username']
-        //     // );
-        //     // return userService.getUser().subscribe((loggedUser) => {
-        //     //     if (
-        //     //         JSON.stringify(loggedUser) ===
-        //     //         JSON.stringify(userFromUrl[0])
-        //     //     ) {
-        //     //         return true;
-        //     //     } else {
-        //     //         router.navigate(['404']);
-        //     //         return false;
-        //     //     }
-        //     // });
-        // } else {
-        //     router.navigate(['404']);
-        //     return false;
-        // }
     });
-
     // after authenticationGuard there was : CanActivateFn
 };
