0c9311b9d6486144fe6035582c915d4dc94bd3d3
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-09-13 07:39:54 +0200

    car info inputs

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-account',
@@ -8,6 +9,21 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class AccountComponent {
     constructor(private userService: UserService) {}
+
+    offerForm = new FormGroup({
+        carBrand: new FormControl('', [Validators.required]),
+        carModel: new FormControl('', [Validators.required]),
+        gearboxType: new FormControl('', [Validators.required]),
+        availableSeats: new FormControl('', [Validators.required]),
+        fuelType: new FormControl('', [Validators.required]),
+        productionYear: new FormControl('', [Validators.required]),
+        // imgs: new FormControl('', [Validators.required]),
+        pickupLocation: new FormControl('', [Validators.required]),
+        availableFor: new FormControl('', [Validators.required]),
+        price: new FormControl('', [Validators.required]),
+    });
+
+    onOfferSubmit() {}
 
     onLogout() {
         this.userService.logout();
