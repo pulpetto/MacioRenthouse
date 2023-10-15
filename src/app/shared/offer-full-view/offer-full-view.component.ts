a9eaebbfa1551b7577136e1fc08d5f58a03e8827
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-10-15 15:39:25 +0200

    offer full view styling

--- src/app/shared/offer-full-view/offer-full-view.component.ts
+++ src/app/shared/offer-full-view/offer-full-view.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-offer-full-view',
-  templateUrl: './offer-full-view.component.html',
-  styleUrls: ['./offer-full-view.component.css']
+    selector: 'app-offer-full-view',
+    templateUrl: './offer-full-view.component.html',
+    styleUrls: ['./offer-full-view.component.css'],
 })
 export class OfferFullViewComponent {
-
+    images = [1, 2, 3, 4];
 }
