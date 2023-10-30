d3a387f2c9fa305044c7be56219fcf11573dbf94
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-10-30 19:45:42 +0100

    correct dropdown skeleton

--- src/app/pages/user/offers-preview/offers-preview.component.ts
+++ src/app/pages/user/offers-preview/offers-preview.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
     templateUrl: './offers-preview.component.html',
     styleUrls: ['./offers-preview.component.css'],
 })
-export class OffersPreviewComponent {}
+export class OffersPreviewComponent {
+    toggleExpand = function (element: any) {
+        if (!element.style.height || element.style.height == '0px') {
+            element.style.height =
+                Array.prototype.reduce.call(
+                    element.childNodes,
+                    function (p, c) {
+                        return p + (c.offsetHeight || 0);
+                    },
+                    0
+                ) + 'px';
+        } else {
+            element.style.height = '0px';
+        }
+    };
+}
