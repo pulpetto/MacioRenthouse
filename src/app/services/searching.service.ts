d4f52d2aaeff2910a988bd1eea132dbf62dc42c7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-12-06 11:36:54 +0100

    convert to display ready structure

--- src/app/services/searching.service.ts
+++ src/app/services/searching.service.ts
@@ -16,6 +16,12 @@ export class SearchingService {
         'Buick',
     ]);
 
+    // prettier-ignore
+    private suggestionsLetters$ = new BehaviorSubject<{
+        letter: string;
+        match: boolean;
+    }[][] | null>([]);
+
     updateSearchTerm(updatedSearchTerm: string) {
         this.searchTerm$.next(updatedSearchTerm);
 
@@ -31,6 +37,30 @@ export class SearchingService {
                 // add other fixes to the suggestion string
                 return suggestion.toLowerCase().startsWith(searchTermFixed);
             }) || null;
+
+        searchingSuggestionsFiltered?.map((suggestion) => {
+            const suggestionsLetters: { letter: string; match: boolean }[] = [];
+            Array.from(suggestion.toLowerCase()).map((letter) => {
+                const letterObj = {
+                    letter: letter,
+                    match: false,
+                };
+
+                suggestionsLetters.push(letterObj);
+            });
+
+            const searchTermLetters = Array.from(searchTermFixed);
+
+            for (let i = 0; i < searchTermLetters.length; i++) {
+                if (suggestionsLetters[i].letter === searchTermLetters[i]) {
+                    suggestionsLetters[i].match = true;
+                }
+            }
+
+            const updateVal = this.suggestionsLetters$.getValue();
+            updateVal?.push(suggestionsLetters);
+            this.suggestionsLetters$.next(updateVal);
+        });
 
         this.searchingSuggestions$.next(searchingSuggestionsFiltered);
     }
