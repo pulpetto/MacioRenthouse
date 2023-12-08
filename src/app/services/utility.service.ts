194c311aa7ee94f140c3412d1b9557a605ab1f6b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-12-08 06:55:38 +0100

    add random string method

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -13,4 +13,18 @@ export class UtilityService {
 
         return resultString;
     }
+
+    generateRandomString(length: number): string {
+        const characters =
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let result = '';
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * charactersLength);
+            result += characters.charAt(randomIndex);
+        }
+
+        return result;
+    }
 }
