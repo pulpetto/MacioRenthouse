b256711f08f750677dc15b980e271a4de7189cb6
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-12-08 06:48:06 +0100

    refactor searching suggestions method

--- src/app/services/searching.service.ts
+++ src/app/services/searching.service.ts
@@ -1,70 +1,73 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { UserService } from './user.service';
+import { UtilityService } from './utility.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class SearchingService {
     private searchTerm$ = new BehaviorSubject<string | null>(null);
-    private searchingSuggestions$ = new BehaviorSubject<string[] | null>([
-        'Acura',
-        'Alfa Romeo',
-        'Aston Martin',
-        'Audi',
-        'Bentley',
-        'BMW',
-        'Buick',
-    ]);
-
+    private searchingSuggestions$ = new BehaviorSubject<string[] | null>([]);
     // prettier-ignore
     private suggestionsLetters$ = new BehaviorSubject<{
         letter: string;
         match: boolean;
     }[][] | null>([]);
 
+    constructor(
+        private userService: UserService,
+        private utilityService: UtilityService
+    ) {}
+
     updateSearchTerm(updatedSearchTerm: string) {
         this.searchTerm$.next(updatedSearchTerm);
 
         const searchTermFixed = updatedSearchTerm
-            .toLowerCase() // Convert to lowercase
             .trim() // Trim leading and trailing whitespaces
             .replace(/[^\w\s]/gi, '') // Remove non-alphanumeric characters
             .normalize('NFD') // Normalize accents/diacritics
             .replace(/[\u0300-\u036f]/g, ''); // Remove accents/diacritics
 
-        const searchingSuggestionsFiltered =
-            this.searchingSuggestions$.value?.filter((suggestion) => {
-                // add other fixes to the suggestion string
-                return suggestion.toLowerCase().startsWith(searchTermFixed);
-            }) || null;
+        this.userService
+            .getOffersNamesBySearchTerm(
+                this.utilityService.capitalizeEveryWord(searchTermFixed)
+            )
+            .subscribe((searchingSuggestions) => {
+                this.suggestionsLetters$.next([]);
 
-        this.suggestionsLetters$.next([]);
+                searchingSuggestions.map((suggestion) => {
+                    const suggestionsLetters: {
+                        letter: string;
+                        match: boolean;
+                    }[] = [];
+                    Array.from(suggestion.toLowerCase()).map((letter) => {
+                        const letterObj = {
+                            letter: letter,
+                            match: false,
+                        };
 
-        searchingSuggestionsFiltered?.map((suggestion) => {
-            const suggestionsLetters: { letter: string; match: boolean }[] = [];
-            Array.from(suggestion.toLowerCase()).map((letter) => {
-                const letterObj = {
-                    letter: letter,
-                    match: false,
-                };
+                        suggestionsLetters.push(letterObj);
+                    });
 
-                suggestionsLetters.push(letterObj);
+                    const searchTermLetters = Array.from(searchTermFixed);
+
+                    for (let i = 0; i < searchTermLetters.length; i++) {
+                        if (
+                            suggestionsLetters[i].letter ===
+                            searchTermLetters[i]
+                        ) {
+                            suggestionsLetters[i].match = true;
+                        }
+                    }
+
+                    const updateVal = this.suggestionsLetters$.getValue();
+                    updateVal?.push(suggestionsLetters);
+                    this.suggestionsLetters$.next(updateVal);
+                });
+
+                this.searchingSuggestions$.next(searchingSuggestions);
             });
-
-            const searchTermLetters = Array.from(searchTermFixed);
-
-            for (let i = 0; i < searchTermLetters.length; i++) {
-                if (suggestionsLetters[i].letter === searchTermLetters[i]) {
-                    suggestionsLetters[i].match = true;
-                }
-            }
-
-            const updateVal = this.suggestionsLetters$.getValue();
-            updateVal?.push(suggestionsLetters);
-            this.suggestionsLetters$.next(updateVal);
-        });
-
-        this.searchingSuggestions$.next(searchingSuggestionsFiltered);
     }
 
     getSearchTerm(): Observable<string | null> {
