5b77c7dfad8e905a3ad3f3d9bd237ee0c9f6092f
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-12-25 06:58:23 +0100

    add breadcrumb component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,6 +58,7 @@ import { OffersPreviewComponent } from './pages/user/offers-preview/offers-previ
 import { DropdownMenuComponent } from './shared/dropdown-menu/dropdown-menu.component';
 import { FormsModule } from '@angular/forms';
 import { SearchBarComponent } from './shared/search-bar/search-bar.component';
+import { BreadcrumbComponent } from './header/breadcrumb/breadcrumb.component';
 
 @NgModule({
     declarations: [
@@ -85,6 +86,7 @@ import { SearchBarComponent } from './shared/search-bar/search-bar.component';
         OffersPreviewComponent,
         DropdownMenuComponent,
         SearchBarComponent,
+        BreadcrumbComponent,
     ],
     imports: [
         A11yModule,
