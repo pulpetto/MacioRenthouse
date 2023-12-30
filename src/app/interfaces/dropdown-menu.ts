88bb3c697faf5021652f445ae1e7d28f9217bc39
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-12-30 04:56:40 +0100

    add properties for range input

--- src/app/interfaces/dropdown-menu.ts
+++ src/app/interfaces/dropdown-menu.ts
@@ -2,4 +2,9 @@ export interface DropdownMenu {
     dropdownName: string;
     dropdownOptions: string[];
     dropdownMultiselect: boolean;
+    // for range input
+    type?: string;
+    minVal?: number;
+    maxVal?: number;
+    postFix?: string;
 }
