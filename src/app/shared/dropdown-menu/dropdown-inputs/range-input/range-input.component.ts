ca3688259b48e6ca35f370b32aeb2d3b199bb5a0
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-01-01 07:47:40 +0100

    add range input logic

--- src/app/shared/dropdown-menu/dropdown-inputs/range-input/range-input.component.ts
+++ src/app/shared/dropdown-menu/dropdown-inputs/range-input/range-input.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-range-input',
-  templateUrl: './range-input.component.html',
-  styleUrls: ['./range-input.component.css']
+    selector: 'app-range-input',
+    templateUrl: './range-input.component.html',
+    styleUrls: ['./range-input.component.css'],
 })
 export class RangeInputComponent {
+    @Input() minVal!: number;
+    @Input() maxVal!: number;
+    @Input() suffix!: string;
+    @Input() minimalValChange!: number;
 
+    numberInputValue: string = '';
+    rangeInputValue: number = 0;
+
+    onNumberInput() {
+        if (+this.numberInputValue > this.maxVal) {
+            this.numberInputValue = this.maxVal.toString();
+        }
+
+        this.rangeInputValue = +this.numberInputValue;
+    }
+
+    onRangeInput() {
+        if (this.minimalValChange)
+            this.numberInputValue = (
+                Math.round(this.rangeInputValue / this.minimalValChange) *
+                this.minimalValChange
+            ).toString();
+    }
 }
