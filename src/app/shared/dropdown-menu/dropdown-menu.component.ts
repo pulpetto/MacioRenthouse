648347902f6111e65e77a589f5f1ce6782174d6c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-01-02 05:25:25 +0100

    clear all old methods and properties

--- src/app/shared/dropdown-menu/dropdown-menu.component.ts
+++ src/app/shared/dropdown-menu/dropdown-menu.component.ts
@@ -11,37 +11,19 @@ import {
 } from '@angular/core';
 import { UtilityService } from 'src/app/services/utility.service';
 import { NgxMaskDirective } from 'ngx-mask';
+import { CheckboxSelect } from 'src/app/interfaces/dropdown/checkbox-select';
+import { RangeSelect } from 'src/app/interfaces/dropdown/range-select';
+import { DropdownMenu } from 'src/app/interfaces/dropdown/dropdown-menu';
 
 @Component({
     selector: 'app-dropdown-menu',
     templateUrl: './dropdown-menu.component.html',
     styleUrls: ['./dropdown-menu.component.css'],
 })
-export class DropdownMenuComponent implements OnInit {
-    searchTerm: string = '';
+export class DropdownMenuComponent {
     @Input() dropdownName!: string;
-    @Input() dropdownOptions!: string[];
-    @Input() dropdownMultiselect!: boolean;
-    @Input() type: string = 'checkbox';
-    @Input() minVal!: number | undefined;
-    @Input() maxVal!: number | undefined;
-    @Input() suffix!: string | undefined;
-    @Input() minimalValChange: number | undefined;
-    numberInputValue!: string;
-    rangeInputValue: number = 0;
-    dropdownOptionsConverted: {
-        id: string;
-        name: string;
-        checked: boolean;
-    }[] = [];
-    // for searching purposes
-    dropdownOptionsConvertedCopy: {
-        id: string;
-        name: string;
-        checked: boolean;
-    }[] = [];
+    searchTerm: string = '';
     arrowRotated: boolean = false;
-    anyOptionChecked: boolean = false;
     @Output() orderingChangeEvent = new EventEmitter<string>();
     @Output() sortingChangeEvent = new EventEmitter<string>();
     @Output() maxOffersPerPageChangeEvent = new EventEmitter<string>();
@@ -57,50 +39,7 @@ export class DropdownMenuComponent implements OnInit {
     @ViewChild('list', { read: ElementRef, static: false })
     list!: ElementRef;
 
-    constructor(
-        private elementRef: ElementRef,
-        private utilityService: UtilityService,
-        private changeDetectorRef: ChangeDetectorRef
-    ) {}
-
-    ngOnInit() {
-        this.dropdownOptionsConverted = this.dropdownOptions.map(
-            (optionName, index) => ({
-                id: this.utilityService.generateRandomString(10),
-                name: optionName,
-                checked:
-                    index === 0 && this.dropdownMultiselect === false
-                        ? true
-                        : false,
-            })
-        );
-
-        this.dropdownOptionsConvertedCopy = this.dropdownOptionsConverted;
-    }
-
-    onNumberInput() {
-        if (!this.numberInputValue) return;
-
-        if (+this.numberInputValue > 500000) {
-            this.numberInputValue = '500000';
-        }
-
-        this.rangeInputValue = +this.numberInputValue;
-    }
-
-    onRangeInput() {
-        if (this.minimalValChange)
-            this.numberInputValue = (
-                Math.round(this.rangeInputValue / this.minimalValChange) *
-                this.minimalValChange
-            ).toString();
-    }
-
-    updateAnyOptionsCheckedState() {
-        this.anyOptionChecked = this.dropdownOptionsConverted.some(
-            (option) => option.checked
-        );
-    }
+    constructor(private elementRef: ElementRef) {}
 
     toggleExpand = function (element: any) {
         if (!element.style.height || element.style.height == '0px') {
@@ -116,54 +55,4 @@ export class DropdownMenuComponent implements OnInit {
             element.style.height = '0px';
         }
     };
-
-    onDropdownPropertySearch(element: any) {
-        const filteredOptions = this.dropdownOptionsConvertedCopy.filter(
-            (option) => option.name.startsWith(this.searchTerm)
-        );
-
-        this.dropdownOptionsConverted = filteredOptions;
-
-        this.changeDetectorRef.detectChanges();
-        element.style.height =
-            Array.prototype.reduce.call(
-                element.childNodes,
-                function (p, c) {
-                    return p + (c.offsetHeight || 0);
-                },
-                0
-            ) + 'px';
-    }
-
-    onInputClick(option: any, $event: Event) {
-        this.updateAnyOptionsCheckedState();
-
-        if (!this.dropdownMultiselect) {
-            if (option.checked) $event.preventDefault();
-            if (!option.checked) {
-                this.dropdownOptionsConverted.forEach(
-                    (option) => (option.checked = false)
-                );
-                option.checked = true;
-
-                // emitt to the event with name from input to avoid repeating
-                this.orderingChangeEvent.emit(option.name);
-                this.sortingChangeEvent.emit(option.name);
-                this.maxOffersPerPageChangeEvent.emit(option.name);
-            }
-        }
-    }
-
-    checkStateChange() {
-        this.updateAnyOptionsCheckedState();
-    }
-
-    unCheckAllOptions() {
-        if (this.dropdownMultiselect) {
-            this.dropdownOptionsConverted.forEach(
-                (option) => (option.checked = false)
-            );
-        }
-        this.updateAnyOptionsCheckedState();
-    }
 }
