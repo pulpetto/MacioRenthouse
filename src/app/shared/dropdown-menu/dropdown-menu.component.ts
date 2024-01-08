59e11842813ec50189a08a6f17100fd7df5ffece
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-01-08 15:06:54 +0100

    make dropdown width configurable

--- src/app/shared/dropdown-menu/dropdown-menu.component.ts
+++ src/app/shared/dropdown-menu/dropdown-menu.component.ts
@@ -19,6 +19,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class DropdownMenuComponent implements AfterViewInit {
     @Input() dropdownName!: string;
+    @Input() dropdownWidth!: number;
     searchTerm: string = '';
     arrowRotated: boolean = false;
     checkedOptionsCount: number = 0;
