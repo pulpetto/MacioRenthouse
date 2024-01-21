dbb03086513aca980a8e8f3a6339e3323793093c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-01-21 04:54:41 +0100

    add optional properties

--- src/app/interfaces/dropdown/checkbox-select.ts
+++ src/app/interfaces/dropdown/checkbox-select.ts
@@ -1,5 +1,8 @@
+import { FilteringOption } from '../filtering-option';
+
 export interface CheckboxSelect {
     type: 'checkbox';
     dropdownOptions: string[];
     dropdownMultiselect: boolean;
+    filteringOption?: FilteringOption;
 }
--- src/app/interfaces/dropdown/range-select.ts
+++ src/app/interfaces/dropdown/range-select.ts
@@ -1,7 +1,10 @@
+import { FilteringOption } from '../filtering-option';
+
 export interface RangeSelect {
     type: 'range';
     minVal: number;
     maxVal: number;
     suffix: string;
     minimalValChange: number;
+    filteringOption?: FilteringOption;
 }
