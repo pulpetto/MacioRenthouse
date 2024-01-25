b0276defc2f8b972d0d20b9ad917ef2e1104a80d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-01-25 04:52:09 +0100

    change property type

--- src/app/shared/dropdown-menu/dropdown-inputs/range-input/range-input.component.ts
+++ src/app/shared/dropdown-menu/dropdown-inputs/range-input/range-input.component.ts
@@ -12,7 +12,7 @@ export class RangeInputComponent {
     @Input() maxVal!: number;
     @Input() suffix: string = '';
     @Input() minimalValChange!: number;
-    @Input() minOrMax!: string;
+    @Input() minOrMax!: 'min' | 'max';
     @Input() ngxMask!: string;
     @Input() connectedToFilter: RangeFilters | undefined;
     @Output() rangeInputValueChangeEvent = new EventEmitter<number>();
