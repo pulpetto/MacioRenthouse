1be11c19e7badd3e83f51ffc6176fba8f8b3f7f9
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-02-18 18:52:51 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     content: ["./src/**/*.{html,ts}"],
     theme: {
         extend: {
+            animation: {
+                "loop-scroll": "loop-scroll 50s linear infinite",
+                "loop-scroll-2": "loop-scroll-2 50s linear infinite",
+            },
+            keyframes: {
+                "loop-scroll": {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-100%)" },
+                },
+                "loop-scroll-2": {
+                    from: { transform: "translateX(100%)" },
+                    to: { transform: "translateX(0)" },
+                },
+            },
             height: {
                 54: "13.5rem",
             },
