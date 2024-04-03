813f19a0302eb1aea97432ed8bddb6047f5dc537
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-03 06:56:02 +0200

    add bookmarking offer functionality

--- src/app/shared/offer/offer.component.ts
+++ src/app/shared/offer/offer.component.ts
@@ -1,11 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Offer } from 'src/app/interfaces/offer';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-offer',
     templateUrl: './offer.component.html',
     styleUrls: ['./offer.component.css'],
 })
-export class OfferComponent {
+export class OfferComponent implements OnInit {
     @Input() offerData!: Offer;
+    destroyRef = inject(DestroyRef);
+    bookmarVisibility: boolean = false;
+    isBookmared: boolean = false;
+
+    constructor(private userService: UserService) {}
+
+    ngOnInit() {
+        this.userService
+            .getUserFavouriteOffers()
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((offers) => {
+                if (
+                    offers.find(
+                        (offer) =>
+                            JSON.stringify(offer) ===
+                            JSON.stringify(this.offerData)
+                    )
+                )
+                    this.isBookmared = true;
+            });
+    }
+
+    addOrRemoveOfferFromFavourites($event: Event, offer: Offer) {
+        $event.stopPropagation();
+        $event.preventDefault();
+
+        this.isBookmared = !this.isBookmared;
+        this.userService.addOrRemoveOfferFromFavourites(
+            offer,
+            this.isBookmared
+        );
+    }
 }
