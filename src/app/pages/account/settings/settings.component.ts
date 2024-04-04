df6b5a6e9616bfc1d4414db41763909e66862be2
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-04 05:59:01 +0200

    indentation

--- src/app/pages/account/settings/settings.component.ts
+++ src/app/pages/account/settings/settings.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css'],
 })
-export class SettingsComponent {
-
-}
+export class SettingsComponent {}
